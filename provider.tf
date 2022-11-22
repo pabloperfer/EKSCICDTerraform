08951868830102aa39800ddab4bc1d5a1679c433
EC2 Default User <ec2-user@ip-172-31-21-89.ec2.internal>
2022-11-22 20:45:22 +0000

    sd

--- provider.tf
+++ provider.tf
@@ -6,8 +6,8 @@ variable "workspace_iam_roles" {
 }
 
 provider "aws" {
-  assume_role = {
     region = "us-east-1"
+  assume_role = {
     role_arn = "${var.workspace_iam_roles[terraform.workspace]}"
   }
 }
