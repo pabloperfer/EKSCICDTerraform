c97669a2824daf883c2a5b061759e33b529adfdb
EC2 Default User <ec2-user@ip-172-31-21-89.ec2.internal>
2022-11-22 19:50:59 +0000

    sd32

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 data "terraform_remote_state" "eks-cluster-test" {
   backend = "s3"
+  workspace = terraform.workspace
   config = {
     bucket = "ferpablo-terra-bucket"
-    workspace = terraform.workspace
     key    = "eks-cluster-test/terraform.tfstate"
     region = "us-east-1"
     dynamodb_table = "dynamodb-state-locking"
