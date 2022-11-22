84bcc2cec1d64aa375016678046c5378105791db
EC2 Default User <ec2-user@ip-172-31-21-89.ec2.internal>
2022-11-22 20:43:56 +0000

    sd

---
+++ main.tf
@@ -0,0 +1,9 @@
+data "aws_availability_zones" "available" {}
+
+locals {
+  cluster_name = "eks-cluster-${terraform.workspace}"
+}
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+}
