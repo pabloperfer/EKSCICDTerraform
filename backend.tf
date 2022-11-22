5e233bb9dead3e7d56d819b1664536df826cf74d
EC2 Default User <ec2-user@ip-172-31-21-89.ec2.internal>
2022-11-22 20:15:04 +0000

    sad

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ data "terraform_remote_state" "eks-cluster-test" {
   backend = "s3"
   config = {
     bucket = "ferpablo-terra-bucket"
-    key    = "eks-cluster-test/terraform.tfstate"
+    key    = "terraform.tfstate"
     region = "us-east-1"
     dynamodb_table = "dynamodb-state-locking"
   }
